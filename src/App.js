f5467e6fbd8ab9ad88ce92d2fe097335f88724bf
Jay Lord P. Torres <isens.jaylord@gmail.com>
2019-03-07 22:02:57 +0800

    app.js add material provider

--- src/App.js
+++ src/App.js
@@ -1,41 +1,50 @@
-import React, { Component } from 'react';
-import Router from 'route-lite';
-import {IntlProvider} from 'react-intl';
-import { Sidemenu, FullScreenLoading, Notifier, AlertDialog } from './components'
-import Splash  from './pages/Splash/Splash'
-import './App.css';
+import React, { Component } from 'react'
+import { ThemeProvider } from '@material-ui/styles'
+import Router from 'route-lite'
+import { IntlProvider } from 'react-intl'
+import Splash from './pages/Splash/Splash'
+import {
+  Sidemenu,
+  FullScreenLoading,
+  Notifier,
+  AlertDialog
+} from './components'
+import './App.css'
+
+const theme = {}
 
 class App extends Component {
-
   constructor(props) {
-    super(props);
+    super(props)
 
     this.state = {
-      locale: 'en',
-    };
+      locale: 'en'
+    }
   }
   componentDidMount() {
-    // 
+    //
   }
 
   render() {
     const { locale } = this.state
 
     return (
-      <IntlProvider locale={locale}>
-        <div className="App">
-          <Sidemenu/>
-          <Notifier/>
-          <FullScreenLoading/>
-          <AlertDialog/>
+      <ThemeProvider theme={theme}>
+        <IntlProvider locale={locale}>
+          <div className="App">
+            <Sidemenu />
+            <Notifier />
+            <FullScreenLoading />
+            <AlertDialog />
 
-          <Router>
-            <Splash/>
-          </Router>
-        </div>
-      </IntlProvider>
-    );
+            <Router>
+              <Splash />
+            </Router>
+          </div>
+        </IntlProvider>
+      </ThemeProvider>
+    )
   }
 }
 
-export default App;
+export default App
