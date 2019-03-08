5f01dd7436ac94f362e099d1a6de0f89cb1e4952
Jay Lord P. Torres <isens.jaylord@gmail.com>
2019-03-09 00:57:10 +0800

    snackbar

---
+++ src/components/Snackbar/Snackbar.jsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Snackbar from '@material-ui/core/Snackbar';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+
+import styles from './styles'
+
+let showSnackbarFn;
+
+class Snackbars extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      open: false,
+      message: '',
+      variant: 'success'
+    }
+  }
+
+  componentDidMount() {
+    showSnackbarFn = this.onHandleOpen.bind(this);
+  }
+
+  onHandleOpen(payload) {
+    this.setState({
+      open: true,
+      ...payload
+    })
+  }
+
+  handleClick = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    this.setState({ open: false });
+  };
+
+render() {
+    const { classes } = this.props;
+    return (
+      <div>
+        <Snackbar
+        open={this.state.open}
+
+      aria-describedby="client-snackbar"
+          autoHideDuration={3000}
+          onClose={this.handleClose}
+      message={
+        <span id="client-snackbar" className={classes.message}>
+          {this.state.message}
+        </span>
+      }
+      action={[
+        <IconButton
+          key="close"
+          aria-label="Close"
+          color="inherit"
+          className={classes.close}
+          onClick={this.handleClose}
+        >
+          <CloseIcon className={classes.icon} />
+        </IconButton>,
+      ]}
+
+ 
+        />
+      </div>
+    );
+  }
+}
+
+
+export function showSnackbar(payload) {
+  showSnackbarFn(payload);
+}
+
+export default withStyles(styles)(Snackbars);
