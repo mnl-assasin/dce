65dc54a514cfe903693c3249ef4023c7a57ee539
Jay Lord P. Torres <isens.jaylord@gmail.com>
2019-03-09 20:08:22 +0800

    clean

--- src/pages/Settings/Settings.js
+++ src/pages/Settings/Settings.js
@@ -1,35 +1,29 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import Checkbox from '@material-ui/core/Checkbox';
-import IconButton from '@material-ui/core/IconButton';
-import CommentIcon from '@material-ui/icons/Comment';
-import Divider from '@material-ui/core/Divider';
-import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
-import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import { withStyles } from '@material-ui/core/styles'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import Divider from '@material-ui/core/Divider'
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight'
 
 import BasePage from '../../common/BasePage'
 import { Page } from '../../common'
-import { Navbar } from '../../components';
+import { Navbar } from '../../components'
 import { withAppContext } from '../../services/Providers/AppStateContext'
-import { getNetworkById } from '../../services/storage/collections/network'
 
-import styles from './styles';
-import './Settings.css';
+import styles from './styles'
 
 // this will ensure that the render will not make new init every render for child
 // this must be partenered will pureComponent
 const variables = {
-      sLoginSecurityV: 'password etc',
+  sLoginSecurityV: 'password etc'
 }
 
 class Settings extends BasePage {
-  title = "Settings"
+  title = 'Settings'
   settingItems = [
     {
       title: 'Login and Security',
@@ -38,38 +32,43 @@ class Settings extends BasePage {
     },
     {
       title: 'Network',
-      getValue: () => this.props.AppContext[this.constants.storage.ACTIVE_PROVIDER_NAME],
+      getValue: () =>
+        this.props.AppContext[this.constants.storage.ACTIVE_PROVIDER_NAME],
       onClick: () => this.navigate(this.constants.route.NETWORK)
     }
   ]
 
-  render () {
-    const { classes } = this.props;
+  render() {
+    const { classes } = this.props
     return (
       <Page>
         <Navbar title={this.title} backButton />
         <div>
           <List className={classes.root}>
-            {this.settingItems.map((item, index ) => (
-              <React.Fragment key={index} >
-              <ListItem key={index}  button onClick={item.onClick}>
-                <ListItemText primary={item.title} secondary={item.getValue()} />
-                <ListItemIcon>
-                  <KeyboardArrowRight />
-                </ListItemIcon>
-              </ListItem><Divider />
+            {this.settingItems.map((item, index) => (
+              <React.Fragment key={index}>
+                <ListItem key={index} button onClick={item.onClick}>
+                  <ListItemText
+                    primary={item.title}
+                    secondary={item.getValue()}
+                  />
+                  <ListItemIcon>
+                    <KeyboardArrowRight />
+                  </ListItemIcon>
+                </ListItem>
+                <Divider />
               </React.Fragment>
             ))}
           </List>
         </div>
       </Page>
-    );
+    )
   }
 }
 
 Settings.propTypes = {
-   AppContext: PropTypes.object.isRequired,  // withAppContext
-   classes: PropTypes.object.isRequired,     // withStyles
-};
+  AppContext: PropTypes.object.isRequired, // withAppContext
+  classes: PropTypes.object.isRequired // withStyles
+}
 
-export default withStyles(styles)(withAppContext(Settings));
+export default withStyles(styles)(withAppContext(Settings))
