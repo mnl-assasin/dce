2268f15d93a327187a47aa0dfad53cd3b61139eb
Mykel Neds Leaño <mykelneds.03@gmail.com>
2019-03-10 19:57:19 +0800

    Removed Login and Security in Setings Page

--- src/pages/Settings/Settings.js
+++ src/pages/Settings/Settings.js
@@ -1,45 +1,45 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
 
-import { withStyles } from '@material-ui/core/styles'
-import List from '@material-ui/core/List'
-import ListItem from '@material-ui/core/ListItem'
-import ListItemText from '@material-ui/core/ListItemText'
-import ListItemIcon from '@material-ui/core/ListItemIcon'
-import Divider from '@material-ui/core/Divider'
-import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight'
+import { withStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import Divider from "@material-ui/core/Divider";
+import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight";
 
-import BasePage from '../../common/BasePage'
-import { Page } from '../../common'
-import { Navbar } from '../../components'
-import { withAppContext } from '../../services/Providers/AppStateContext'
+import BasePage from "../../common/BasePage";
+import { Page } from "../../common";
+import { Navbar } from "../../components";
+import { withAppContext } from "../../services/Providers/AppStateContext";
 
-import styles from './styles'
+import styles from "./styles";
 
 // this will ensure that the render will not make new init every render for child
 // this must be partenered will pureComponent
 const variables = {
-  sLoginSecurityV: 'password etc'
-}
+  sLoginSecurityV: "password etc"
+};
 
 class Settings extends BasePage {
-  title = 'Settings'
+  title = "Settings";
   settingItems = [
+    // {
+    //   title: 'Login and Security',
+    //   getValue: () => variables.sLoginSecurityV,
+    //   onClick: () => this.navigate(this.constants.route.SECURITY)
+    // },
     {
-      title: 'Login and Security',
-      getValue: () => variables.sLoginSecurityV,
-      onClick: () => this.navigate(this.constants.route.SECURITY)
-    },
-    {
-      title: 'Network',
+      title: "Network",
       getValue: () =>
         this.props.AppContext[this.constants.storage.ACTIVE_PROVIDER_NAME],
       onClick: () => this.navigate(this.constants.route.NETWORK)
     }
-  ]
+  ];
 
   render() {
-    const { classes } = this.props
+    const { classes } = this.props;
     return (
       <Page>
         <Navbar title={this.title} backButton />
@@ -62,13 +62,13 @@ class Settings extends BasePage {
           </List>
         </div>
       </Page>
-    )
+    );
   }
 }
 
 Settings.propTypes = {
   AppContext: PropTypes.object.isRequired, // withAppContext
   classes: PropTypes.object.isRequired // withStyles
-}
+};
 
-export default withStyles(styles)(withAppContext(Settings))
+export default withStyles(styles)(withAppContext(Settings));
