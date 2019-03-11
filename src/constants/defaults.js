fb6421123fda854e9bbd9ff3aa298b5b6d0a8705
Jay Lord P. Torres <isens.jaylord@gmail.com>
2019-03-11 21:18:14 +0800

    default network to mainet

--- src/constants/defaults.js
+++ src/constants/defaults.js
@@ -3,6 +3,6 @@
 // bause default is a reserve word
 //
 export const activeProvider = {
-    'ACTIVE_PROVIDER_ID': '_server_ropsten',
-    'ACTIVE_PROVIDER_NAME': 'Ropsten Testnet'
+    'ACTIVE_PROVIDER_ID': '_server_mainnet',
+    'ACTIVE_PROVIDER_NAME': 'Mainnet'
 }
