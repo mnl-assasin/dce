2ba8989a353dd3bba35d0d2cf7b953770d29585d
Jay Lord P. Torres <isens.jaylord@gmail.com>
2019-03-12 23:23:22 +0800

    page: dashboard

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -4,15 +4,20 @@ import PropTypes from "prop-types"
 import { withStyles } from "@material-ui/core/styles";
 import Button from '@material-ui/core/Button';
 import Icon from '@material-ui/core/Icon';
+import Typography from '@material-ui/core/Typography'
 
 import BasePage from "../../common/BasePage";
-import { Page, Row, Col } from '../../common'
+// import ThemeIcon from '../../common/Icon'
+import { Page, Row, Col, Icon as ThemeIcon } from '../../common'
 import { Navbar } from '../../components';
 import { goTo } from '../../services/navigation';
 import { withAppContext } from "../../services/Providers/AppStateContext";
 
 import styles from './styles';
 import './Dashboard.scss';
+
+import EtherImage from './icon_ether.png'
+import BitImage from './icon_bitcoin.png'
 
 class Dashboard extends BasePage {
   title = 'Dashboard'
@@ -27,36 +32,60 @@ class Dashboard extends BasePage {
       items: [
         {
           model: {},
-          label: '123',
-          icon: 'card_travel',
+          isWallet: true,
+          amount: '0.123432',
+          value: '0.123432',
+          iconComponent: <ThemeIcon src={EtherImage} size="70" />,
           onClick: (model) => {
             goTo('Wallet', model)
           }
         },
         {
           model: {},
-          label: '123',
-          icon: 'card_travel',
+          isWallet: true,
+          amount: '0.123432',  
+          value: '0.123432',
+          iconComponent: <ThemeIcon size="70" src={EtherImage} />,
           onClick: (model) => {
             goTo('Wallet', model)
           }
         },
         {
           model: {},
-          label: '123',
-          icon: 'card_travel',
+          isWallet: true,
+          amount: '2.123432',  
+          value: '0.123432',
+          iconComponent: <ThemeIcon size="70" src={EtherImage} />,
           onClick: (model) => {
             goTo('Wallet', model)
           }
         },
         {
           model: {},
-          label: '123',
-          icon: 'card_travel',
+          isWallet: true,
+          amount: '4.123432',  
+          value: '100.123432',
+          iconComponent: <ThemeIcon size="70" src={BitImage} />,
           onClick: (model) => {
             goTo('Wallet', model)
           }
-        }
+        },
+        // {
+        //   model: {},
+        //   label: '123',
+        //   iconComponent: <ThemeIcon size="70" iconName="card_travel"></ThemeIcon>,
+        //   onClick: (model) => {
+        //     goTo('Wallet', model)
+        //   }
+        // },
+        // {
+        //   model: {},
+        //   label: '1234',
+        //   iconComponent: <ThemeIcon size="70" iconName="card_travel" />,
+        //   onClick: (model) => {
+        //     goTo('Wallet', model)
+        //   }
+        // }
       ]
     };
 
@@ -75,18 +104,39 @@ class Dashboard extends BasePage {
   }
 
   onRenderWidget(item, i) {
+    //
+    // when item list icon is a wallet that have 2 lables, amout and value
+    //
+    if (item.isWallet) {
+      return (
+        <div className="Dashboard--widget" key={i} onClick={this.onClickItem.bind(this, item)}>
+          <div>
+            {item.iconComponent}
+          </div>
+          <Typography align="center">
+              { item.amount }
+            </Typography>
+          <Typography  gutterBottom align="center">
+              ${ item.value }
+          </Typography>
+        </div>
+      )
+    }
+
+    //
+    // only icon and 1 label
+    //
     return (
       <div className="Dashboard--widget" key={i} onClick={this.onClickItem.bind(this, item)}>
         <div>
-          <Icon>{item.icon}</Icon>
+          {item.iconComponent}
         </div>
-        <div>
-          <span>
-            { item.label }
-          </span>
-        </div>
+          <Typography  gutterBottom align="center">
+              { item.label }
+          </Typography>
       </div>
     )
+
   }
 
   onRenderAddWidget() {
@@ -94,6 +144,7 @@ class Dashboard extends BasePage {
       model: {},
       label: 'Add new wallet',
       icon: 'add_circle_outline',
+      iconComponent: <ThemeIcon size="70" iconName="add_circle_outline" />,
       onClick: () => {
         this.setState({
           items: [
@@ -102,6 +153,7 @@ class Dashboard extends BasePage {
               model: {},
               label: '123',
               icon: 'card_travel',
+              iconComponent: <ThemeIcon size="70" src={EtherImage} />,
               onClick: (model) => {
                 goTo('Wallet', model)
                 // goTo('Wallet', model)
