980208b54c6b0951e67644efc75ed0f956c2d904
Jay Lord P. Torres <isens.jaylord@gmail.com>
2019-03-13 04:48:30 +0800

    page: backupRestore

--- src/pages/RestoreBackup/RestoreBackup.js
+++ src/pages/RestoreBackup/RestoreBackup.js
@@ -1,42 +1,66 @@
-import React, { Component } from 'react';
-import { goTo } from '../../services/navigation';
-import { Page, Row, Col, Input, Button } from '../../common'
-import { Navbar, alertDialog } from '../../components';
-import { WalletRepository } from 'dapper-node';
-import './RestoreBackup.css';
+import React from 'react'
+import PropTypes from "prop-types"
+import { Wallet } from 'dapper-js'
 
-class RestoreBackup extends Component {
-  constructor(props) {
-    super(props)
+import { withStyles } from "@material-ui/styles"
+import TextField from "@material-ui/core/TextField"
+import Button from "@material-ui/core/Button"
 
-    this.state = {
-      phrase: ''
-    }
+import BasePage from "../../common/BasePage"
+import { Row, Col } from "../../common"
+import { goTo } from '../../services/navigation'
+import Page from '../../layout/Page'
+import { Navbar, alertDialog } from '../../components'
+import { inputTypes } from "../../constants/types"
+import { withAppContext } from "../../services/Providers/AppStateContext"
 
+import styles from "./styles"
+import './RestoreBackup.css'
+
+class RestoreBackup extends BasePage {
+  title = 'RestoreBackup'
+  defaults = BasePage.constants.defaults
+  storage = BasePage.constants.storage
+
+  state = {
+    phrase: ''
   }
 
-  async onClickSubmit() {
-    let result = WalletRepository.restore({
-      mnemonic: this.state.phrase
-    })
+  onClickSubmit = async () => {
+    let wallet
+    try {
+      wallet = await Wallet.ethers.restore({mnemonic: this.state.phrase})
+      if (wallet.code === 200) {
+        this.props.AppContext.persist({
+          [this.storage.WALLET_MNEMONIC]: wallet.data.mnemonic,
+          [this.storage.WALLET_ADDRESS]: wallet.data.address,
+          [this.storage.WALLET_PRIVATE_KEY]: wallet.data.privateKey,
+          [this.storage.WALLET_PUBLIC_KEY]: wallet.data.publicKey
+        })
+        // redir
+        goTo('NominatePassword', wallet)
 
-    if (result) {
-      goTo('NominatePassword', result)
-    } else {
+      } else {
+        throw new Error('no valid wallet data')
+      }
+    } catch(e) {
+      console.log('error in restoring backup', e)
       alertDialog({
         title: 'Oopss',
         message: 'It seems like you have entered a wrong mnemonic phrase. please try again'
       })
     }
+     
   }
 
-  onHandlePhraseValue(e) {
+  onHandlePhraseValue = async (e) => {
     this.setState({
       phrase: e.target.value
     })
   }
 
   render() {
+    const { classes } = this.props;
     return (
       <Page className="RestoreBackup">
         <Navbar backButton={true}/>
@@ -47,9 +71,25 @@ class RestoreBackup extends Component {
           </Row>
 
           <Col flex="1">
-            <label>Enter backup phrase</label>
-            <Input style={{height: '20px'}} value={this.state.phrase} onChange={this.onHandlePhraseValue.bind(this)} />
-            <Button onClick={this.onClickSubmit.bind(this)}> Submit </Button>
+            <TextField
+              label="Enter backup phrase"
+              margin="normal"
+              variant="outlined"
+              className={classes.textField}
+              value={this.state.phrase} 
+              onChange={this.onHandlePhraseValue}
+              type={inputTypes.text}
+            />
+            <div className={classes.buttonHolder}>
+              <Button
+                variant="outlined"
+                color="primary"
+                size="large"
+                onClick={this.onClickSubmit}
+              >
+                Submit
+              </Button>
+            </div>
           </Col>
         </div>
       </Page>
@@ -57,4 +97,9 @@ class RestoreBackup extends Component {
   }
 }
 
-export default RestoreBackup;
+RestoreBackup.propTypes = {
+  AppContext: PropTypes.object.isRequired, // withAppContext
+  classes: PropTypes.object.isRequired // withStyles
+}
+
+export default withStyles(styles)(withAppContext(RestoreBackup))
