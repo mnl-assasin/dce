a2fe099411e8b995d70bb4e849a4b9bb4700d3ec
Jay Lord P. Torres <isens.jaylord@gmail.com>
2019-03-13 04:50:56 +0800

    page: test

--- src/pages/TestPage/TestPage.jsx
+++ src/pages/TestPage/TestPage.jsx
@@ -1,21 +1,38 @@
-import React, { Component } from 'react'
+import React  from 'react'
+import PropTypes from "prop-types";
 
+import { Wallet } from 'dapper-js'
 
+import BasePage from "../../common/BasePage"
 import { goTo } from '../../services/navigation'
 import Button from "@material-ui/core/Button";
 import Divider from "@material-ui/core/Divider";
 import Page from '../../layout/Page'
 import { Navbar } from '../../components'
-import { Wallet } from 'dapper-js'
+import { withAppContext } from "../../services/Providers/AppStateContext";
 
-class TestPage extends Component {
+
+// from doc
+const mnemonic = "crunch soldier universe crunch flight clip urge chalk giant silver rug tank"
+
+
+
+class TestPage extends BasePage {
+  title = 'TestPage'
+  // store = BasePage.store
+  defaults = BasePage.constants.defaults
+  storage = BasePage.constants.storage
+
+   componentDidMount () {
+     console.log(this.props)
+   }
 
   _onGetStarted = async () => {
     console.log(Wallet)
     Wallet.ethers
       .restore({
         mnemonic:
-          "crunch soldier universe crunch flight clip urge chalk giant silver rug tank1"
+          "crunch soldier universe crunch flight clip urge chalk giant silver rug tank"
       })
       .then(data => {
         console.log(data);
@@ -29,18 +46,51 @@ class TestPage extends Component {
       }
 
   _onRestoreBackup = async() => {
-    const walletHelper = Wallet.ethers.getHelper()
-    console.log(walletHelper)
-    
-    const getProvider = await walletHelper.getProvider()
-console.log(getProvider)
+    let wallet
+    try {
+      wallet = await Wallet.ethers.restore({mnemonic})
+      if (wallet.code === 200) {
+        this.props.AppContext.persist({
+          [this.storage.WALLET_MNEMONIC]: wallet.data.mnemonic,
+          [this.storage.WALLET_ADDRESS]: wallet.data.address,
+          [this.storage.WALLET_PRIVATE_KEY]: wallet.data.privateKey,
+          [this.storage.WALLET_PUBLIC_KEY]: wallet.data.publicKey
+        })
 
+      } else {
+        throw new Error('no valid wallet data')
+      }
+    } catch(e) {
+      console.log('error in restoring backup', e)
+    }
+     
+     console.log(wallet)
   }
 
   goto(str) {
     goTo(str)
   }
 
+  loadAppState = () => {
+     console.log(this.props.AppContext)
+   
+  }
+
+
+  printWallet = async() => {
+    const walletHelper = Wallet.ethers.getHelper()
+
+    // getting balance
+    const balance = await  Wallet.ethers.balance({
+      provider: "mainnet",
+        address: this.props.AppContext[this.storage.WALLET_ADDRESS]
+      })
+     console.log(balance)
+     console.log(Wallet.ethers)
+     console.log(walletHelper)
+     console.log(walletHelper.getProvider())
+  
+  }
   render() {
     return (
       <Page>
@@ -55,6 +105,15 @@ console.log(getProvider)
               <Button variant="outlined" onClick={this._onRestoreBackup} fullWidth>
                 Restore Backup
               </Button>
+        <Divider />
+              <Button variant="outlined" onClick={() => console.log(this.props.AppContext)} fullWidth>
+                loadAppState
+              </Button>
+        <Divider />
+              <Button variant="outlined" onClick={this.printWallet} fullWidth>
+                print wallet
+              </Button>
+
 
         </div>
       </Page>
@@ -62,4 +121,9 @@ console.log(getProvider)
   }
 }
 
-export default TestPage
+TestPage.propTypes = {
+  AppContext: PropTypes.object.isRequired, // withAppContext
+  // classes: PropTypes.object.isRequired // withStyles
+};
+
+export default withAppContext(TestPage)
