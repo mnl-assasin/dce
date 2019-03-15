607676f60cab8f1b035b6ef2f194b18b96779e2e
Jay Lord P. Torres <isens.jaylord@gmail.com>
2019-03-16 06:59:00 +0800

    helper: setBalance

---
+++ src/hof/set_balance.js
@@ -0,0 +1,39 @@
+import { Wallet } from 'dapper-js'
+import { emptyMethod } from '../helper/function'
+
+/**
+ *
+ * @require this.storage
+ * @require this.props.AppContext
+ * @require this.props.AppContext
+ *
+ */
+
+export default (context) => async (
+  ACTIVE_PROVIDER_ID,
+  WALLET_ADDRESS,
+  onSuccess = emptyMethod,
+  onError = emptyMethod) => {
+  try {
+    if (!ACTIVE_PROVIDER_ID) {
+      throw new Error('invalid provider')
+    }
+    if (!ACTIVE_PROVIDER_ID) {
+      throw new Error('invalid provider')
+    }
+    const request = await  Wallet.ethers.balance({
+      provider: ACTIVE_PROVIDER_ID,
+      address: WALLET_ADDRESS
+    })
+    if (request.code === 200) {
+      // converted amount price is a component value.
+      // so success must handle by the caller
+      return onSuccess(request.data.balance)
+    } else {
+      throw new Error('code not 200')
+    }
+  } catch (e) {
+    console.log('error in getting wallet balance: ', e)
+    return onError(e)
+  }
+}
