dbf32791e96c43e60d8b2fff255a397861e4687c
Jay Lord P. Torres <isens.jaylord@gmail.com>
2019-03-16 03:03:53 +0800

    Page: improve capabilities

--- src/layout/Page/Page.jsx
+++ src/layout/Page/Page.jsx
@@ -1,16 +1,29 @@
 import React from 'react'
 import Proptypes from 'prop-types'
 
-const component = ({ classes, ...props }) => (
-    <div className={classes.root} {...props} />
+import { Navbar } from '../../components'
+
+const component = ({ classes, children, navigationProps, containerProps, ...props }) => (
+    <div className={classes.root} {...props}>
+      <Navbar {...navigationProps} />
+      <div className={classes.container}  {...containerProps} >
+        {children}
+      </div>
+    </div>
 )
 
 component.defaultProps = {
   classes: {},
+  navigationProps: {},
+  containerProps: {},
+  children: null
 }
 
 component.propTypes = {
-  classes: Proptypes.object,
+  classes: Proptypes.object,  // withStyles
+  children: Proptypes.node,
+  navigationProps: Proptypes.object,
+  containerProps: Proptypes.object,
 }
 
 export default component
--- src/layout/Page/styles.js
+++ src/layout/Page/styles.js
@@ -1,8 +1,14 @@
 export default theme  => ({
   root: {
     width: '100%',
-    maxWidth: 360,
   	height: '100%',
+    maxWidth: 360,
     backgroundColor: theme.palette.background.paper,
   },
-});
+  container: {
+    padding: 10,
+    flex: 1,
+    flexDirection: 'column',
+    display: 'flex'
+  },
+})
