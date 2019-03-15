af0a9e9f95ffbcbb3efb7b0c74cf9af4886c68b9
Jay Lord P. Torres <isens.jaylord@gmail.com>
2019-03-16 07:02:34 +0800

    testpage

--- src/pages/TestPage/TestPage.jsx
+++ src/pages/TestPage/TestPage.jsx
@@ -1,21 +1,20 @@
 import React  from 'react'
 import PropTypes from "prop-types";
 
-import { Wallet } from 'dapper-js'
+import { Wallet, Transaction } from 'dapper-js'
 
 import BasePage from "../../common/BasePage"
 import { goTo } from '../../services/navigation'
 import Button from "@material-ui/core/Button";
 import Divider from "@material-ui/core/Divider";
 import Page from '../../layout/Page'
+import setEtherPrice from '../../hof/set_ether_price'
+import setBalance from '../../hof/set_balance'
 import { Navbar } from '../../components'
 import { withAppContext } from "../../services/Providers/AppStateContext";
 
-
 // from doc
 const mnemonic = "crunch soldier universe crunch flight clip urge chalk giant silver rug tank"
-
-
 
 class TestPage extends BasePage {
   title = 'TestPage'
@@ -23,6 +22,18 @@ class TestPage extends BasePage {
   defaults = BasePage.constants.defaults
   storage = BasePage.constants.storage
 
+  navigationProps = {
+    title: this.title
+  }
+
+  // hof
+  setEtherPrice = setEtherPrice(this)
+  setBalance = setBalance(this)
+
+
+   state = {
+     amount: 'wala'
+   }
    componentDidMount () {
      console.log(this.props)
    }
@@ -63,7 +74,7 @@ class TestPage extends BasePage {
     } catch(e) {
       console.log('error in restoring backup', e)
     }
-     
+
      console.log(wallet)
   }
 
@@ -73,34 +84,68 @@ class TestPage extends BasePage {
 
   loadAppState = () => {
      console.log(this.props.AppContext)
-   
+
   }
 
 
-  printWallet = async() => {
-    const walletHelper = Wallet.ethers.getHelper()
+  printWallet = async () => {
+    const address = '0x0598aC83C088f126B3043059FCfd2E7A5F0886FF'
+    // const walletHelper = Wallet.ethers.getHelper()
 
     // getting balance
     const balance = await  Wallet.ethers.balance({
       provider: "mainnet",
-        address: this.props.AppContext[this.storage.WALLET_ADDRESS]
+      address: address
+        // address: this.props.AppContext[this.storage.WALLET_ADDRESS]
       })
      console.log(balance)
      console.log(Wallet.ethers)
-     console.log(walletHelper)
-     console.log(walletHelper.getProvider())
-  
+     // console.log(walletHelper)
+     // console.log(walletHelper.getProvider())
+     // Wallet.ethers
+    //   .balance({
+    //     provider: "rinkeby",
+    //     address: "0x0598aC83C088f126B3043059FCfd2E7A5F0886FF"
+    //   })
   }
+
+    printBalance = async () => {
+      const address = '0x0598aC83C088f126B3043059FCfd2E7A5F0886FF'
+      const id = 'mainnet'
+      // const balance = await  Wallet.ethers.balance({
+      //   provider: "mainnet",
+      //     address
+      //     // address: this.props.AppContext[this.storage.WALLET_ADDRESS]
+      //   })
+      //  console.log(balance)
+      //  console.log(Wallet.ethers)
+
+       // this.setBalance(this.props.AppContext[this.storage.ACTIVE_PROVIDER_ID], this.props.AppContext[this.storage.WALLET_ADDRESS])
+       this.setBalance(
+         id,
+         address,
+         (value) => this.setState({amount: value})
+       )
+    }
+
+    printEtherPrice = async () => {
+       this.setEtherPrice()
+    }
+
   render() {
+    console.log(this.props.AppContext[this.storage.ETHER_PRICE])
     return (
-      <Page>
-        <Navbar title="Test Page" />
-        <div className="Content">
+      <Page
+        navigationProps={this.navigationProps}
+      >
+      price {this.props.AppContext[this.storage.ETHER_PRICE]}
+      <br />
+      amount {this.state.amount}
         <Divider />
               <Button variant="outlined" color="primary" outline="true" onClick={this._onGetStarted} fullWidth>
                 Get Started
               </Button>
-              
+
         <Divider />
               <Button variant="outlined" onClick={this._onRestoreBackup} fullWidth>
                 Restore Backup
@@ -113,9 +158,10 @@ class TestPage extends BasePage {
               <Button variant="outlined" onClick={this.printWallet} fullWidth>
                 print wallet
               </Button>
-
-
-        </div>
+        <Divider />
+              <Button variant="outlined" onClick={this.printBalance} fullWidth>
+               print Balance
+              </Button>
       </Page>
     )
   }
