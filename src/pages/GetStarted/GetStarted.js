40bb52ba2be5e4b2a98e37894e5d83b45564f357
Jay Lord P. Torres <isens.jaylord@gmail.com>
2019-03-16 03:04:39 +0800

    getStarted: use Page

--- src/pages/GetStarted/GetStarted.js
+++ src/pages/GetStarted/GetStarted.js
@@ -6,47 +6,44 @@ import Button from "@material-ui/core/Button"
 import Typography from '@material-ui/core/Typography'
 
 import BasePage from "../../common/BasePage";
+import Page from '../../layout/Page'
 import { goTo } from '../../services/navigation'
-import { Page } from '../../common'
-import { Navbar } from '../../components'
 
 import styles from "./styles"
 
 class GetStarted extends BasePage {
   title = "Getting Started"
-  _onGetStarted = () => goTo('MnemonicPhrase')
+  navigationProps = {title:this.title}
 
+  _onGetStarted = () => goTo('MnemonicPhrase')
   _onRestoreBackup = () => goTo('RestoreBackup')
 
   render () {
     const { classes } = this.props
 
     return (
-      <Page>
-        <Navbar title={this.title} />
-        <div className={classes.container}>
-          <div className={classes.logo}>
-            <Typography variant="h3"></Typography>
-          </div>
-          <div className={classes.buttonGroup}>
-            <Button
-              variant="outlined"
-              color="primary"
-              size="large"
-              onClick={this._onGetStarted}
-            >
-              Get Started
-            </Button>
-            <br />
-            <Button
-              variant="outlined"
-              color="primary"
-              size="large"
-              onClick={this._onRestoreBackup}
-            >
-              Restore Backup
-            </Button>
-          </div>
+      <Page navigationProps={this.navigationProps}>
+        <div className={classes.logo}>
+          <Typography variant="h3"></Typography>
+        </div>
+        <div className={classes.buttonGroup}>
+          <Button
+            variant="outlined"
+            color="primary"
+            size="large"
+            onClick={this._onGetStarted}
+          >
+            Get Started
+          </Button>
+          <br />
+          <Button
+            variant="outlined"
+            color="primary"
+            size="large"
+            onClick={this._onRestoreBackup}
+          >
+            Restore Backup
+          </Button>
         </div>
       </Page>
     )
