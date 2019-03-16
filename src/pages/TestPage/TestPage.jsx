d44048aefbd0891119be423a99664e74616d8027
Jay Lord P. Torres <isens.jaylord@gmail.com>
2019-03-16 12:22:48 +0800

    package: dapper: test:

--- src/pages/TestPage/TestPage.jsx
+++ src/pages/TestPage/TestPage.jsx
@@ -1,7 +1,7 @@
 import React  from 'react'
 import PropTypes from "prop-types";
 
-import { Wallet, Transaction } from 'dapper-js'
+import { Wallet, Transaction } from 'dapper'
 
 import BasePage from "../../common/BasePage"
 import { goTo } from '../../services/navigation'
