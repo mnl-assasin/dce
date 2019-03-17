398bb9f95fb92de6e7fbc680b91b4e8ba4c21b1d
Jay Lord P. Torres <isens.jaylord@gmail.com>
2019-03-17 13:43:16 +0800

    clean: wallet history

--- src/pages/WalletHistory/WalletHistory.js
+++ src/pages/WalletHistory/WalletHistory.js
@@ -1,19 +1,10 @@
-import React, { useState, useEffect, useCallback, useContext } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import moment from 'moment'
-import Button from '@material-ui/core/Button'
-import TextField from '@material-ui/core/TextField'
 import Typography from '@material-ui/core/Typography'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
-
 import Page from '../../layout/Page'
-import restoreBackup from '../../hof/restore_backup'
-import { useTextbox } from '../../hook'
-import { Col } from '../../common'
-import { goTo as navigate } from '../../services/navigation'
-import { alertDialog } from '../../components'
-import { inputTypes } from '../../constants/types'
 import { AppContextObject } from '../../services/Providers/AppStateContext'
 import * as storage from '../../constants/storage'
 import useStyles from './styles'
@@ -41,31 +32,40 @@ const WalletHistory = props => {
         address: appContext[storage.WALLET_ADDRESS]
       })
       .then(data => {
-        console.log(data);
+        console.log(data)
         setHistories(data.data)
       })
       .catch(error => {
-        console.log("Index catch: ");
-        console.log(error);
-      });
+        console.log('Index catch: ')
+        console.log(error)
+      })
   }, [])
-
+  // will be clear later
   return (
     <Page navigationProps={navigationProps}>
       <div className={classes.logo}>
         <Typography variant="h5">ETH</Typography>
       </div>
       <List>
-      {
-        histories.length > 0 ? histories.reverse().map((item, index) => {
-          return <ListItem key={index} >
-          <ListItemText
-                   primary={'' + moment(1552795756).format('LLL') + ' - ' + item.confirmations+ ' - ' + item.value }
-                   secondary={item.from}
-                 />
-          </ListItem>
-        }) : null
-      }
+        {histories.length > 0
+          ? histories.reverse().map((item, index) => {
+              return (
+                <ListItem key={index}>
+                  <ListItemText
+                    primary={
+                      '' +
+                      moment(1552795756).format('LLL') +
+                      ' - ' +
+                      item.confirmations +
+                      ' - ' +
+                      item.value
+                    }
+                    secondary={item.from}
+                  />
+                </ListItem>
+              )
+            })
+          : null}
       </List>
     </Page>
   )
