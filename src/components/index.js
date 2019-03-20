2b814b69fec0c3ee65b5c7bb0682a540b6d153fd
Jay Lord P. Torres <isens.jaylord@gmail.com>
2019-03-21 00:24:53 +0800

    component: index update for primary button

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,13 @@
 import Navbar from './Navbar/Navbar'
 import Sidemenu from './Sidemenu/Sidemenu'
-import FullScreenLoading, { showLoading, hideLoading } from './FullScreenLoading/FullScreenLoading'
-import Notifier, { openSnackbar } from './Notifier/Notifier';
-import AlertDialog, { alertDialog } from './AlertDialog/AlertDialog';
-import Snackbar, { showSnackbar } from './Snackbar/Snackbar';
+import FullScreenLoading, {
+  showLoading,
+  hideLoading,
+} from './FullScreenLoading/FullScreenLoading'
+import Notifier, { openSnackbar } from './Notifier/Notifier'
+import AlertDialog, { alertDialog } from './AlertDialog/AlertDialog'
+import Snackbar, { showSnackbar } from './Snackbar/Snackbar'
+import PrimaryButton from './PrimaryButton'
 
 export {
   Navbar,
@@ -12,11 +16,12 @@ export {
   Notifier,
   AlertDialog,
   Snackbar,
-
+  PrimaryButton,
+  //
   // functions
   openSnackbar,
   showLoading,
   hideLoading,
   alertDialog,
-  showSnackbar
+  showSnackbar,
 }
