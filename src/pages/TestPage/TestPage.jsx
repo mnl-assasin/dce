98fb2c73dbbcec0fdd68449cd8e5ad0fd6461762
Jay Lord P. Torres <isens.jaylord@gmail.com>
2019-03-23 00:38:54 +0800

    pageTest

--- src/pages/TestPage/TestPage.jsx
+++ src/pages/TestPage/TestPage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import { Wallet, Transaction } from 'dapper'
+import Typography from '@material-ui/core/Typography'
 
 import BasePage from '../../common/BasePage'
 import { goTo } from '../../services/navigation'
@@ -9,23 +10,90 @@ import { PrimaryButton, SmallButton } from '../../components'
 import Button from '@material-ui/core/Button'
 import { withAppContext } from '../../services/Providers/AppStateContext'
 
+import FabButton from '../../components/FabButton'
+import Page from '../../layout/Page'
+import Padder from '../../layout/Padder'
+import CardWallet from '../../layout/CardWallet'
+import Tab from '../../layout/Tab'
+
+import Fab from '@material-ui/core/Fab'
+export const titleStyle = {
+  textTransform: 'uppercase',
+  fontSize: 12,
+  fontWeight: 'bolder',
+}
+export const subTitleStyle = {
+  fontSize: 12,
+  color: 'gray',
+  fontWeight: 'bolder',
+}
 class TestPage extends BasePage {
   render() {
     return (
-      <div>
-        <br />
-        <br />
-        <br />
-        <div style={{ width: '250px' }}>
-          <SmallButton count="9" title="SmallButton" />
-        </div>
-        <div style={{ width: '180px' }}>
-          <SmallButton count="4" title="PrimaryButton" />
-        </div>
-        <PrimaryButton fullWidth type="secondary">
-          do it later
-        </PrimaryButton>
-      </div>
+      <Page>
+        <Tab
+          title="coins"
+          subTitle="$3214.23"
+          renderButton={
+            <React.Fragment>
+              <Padder size={1} horizontal>
+                <FabButton color="red" type="pink" size="medium">
+                  +
+                </FabButton>
+              </Padder>
+              <Padder size={1} horizontal>
+                <FabButton color="red" type="primary" size="medium">
+                  l
+                </FabButton>
+              </Padder>
+              <Padder size={1} horizontal>
+                <FabButton color="red" type="third" size="medium">
+                  A
+                </FabButton>
+              </Padder>
+            </React.Fragment>
+          }
+        >
+          <CardWallet
+            title="$231231.3123"
+            subTitle="@jaylordTorres"
+            componentIcon={
+              <FabButton color="red" type="third" size="small">
+                l
+              </FabButton>
+            }
+          />
+          <CardWallet
+            title="$2311.312233"
+            subTitle="@Torresjaylord"
+            componentIcon={
+              <FabButton color="red" type="third" size="small">
+                l
+              </FabButton>
+            }
+          />
+          <CardWallet
+            title="$2311.312233"
+            subTitle="@Torresjaylord"
+            componentIcon={
+              <FabButton color="red" type="third" size="small">
+                E
+              </FabButton>
+            }
+          />
+        </Tab>
+
+        <Padder size={2} vertical>
+          <PrimaryButton fullWidth type="primary">
+            record recovery
+          </PrimaryButton>
+        </Padder>
+        <Padder size={2} vertical>
+          <PrimaryButton fullWidth type="secondary">
+            backup on chain
+          </PrimaryButton>
+        </Padder>
+      </Page>
     )
   }
 }
