74b58ed035068d093fbc5795449725b5e6a0d006
Jay Lord P. Torres <isens.jaylord@gmail.com>
2019-03-23 15:26:53 +0800

    layout: Page: fix spacing

--- src/layout/Page/styles.js
+++ src/layout/Page/styles.js
@@ -1,14 +1,14 @@
-export default theme  => ({
+export default theme => ({
   root: {
     width: '100%',
-  	height: '100%',
+    height: '100%',
     maxWidth: 360,
     backgroundColor: theme.palette.background.paper,
   },
   container: {
-    padding: 10,
+    padding: '14px 26px ',
     flex: 1,
     flexDirection: 'column',
-    display: 'flex'
+    display: 'flex',
   },
 })
