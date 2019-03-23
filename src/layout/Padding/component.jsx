2daf669d871f92c6815e2f59897c5fa97bf4ad6f
Jay Lord P. Torres <isens.jaylord@gmail.com>
2019-03-23 14:35:02 +0800

    component: revise padding component

---
+++ src/layout/Padding/component.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Proptypes from 'prop-types'
+import { getStyle } from './styles'
+
+const component = ({
+  children,
+  style,
+  size,
+  all = undefined,
+  left = undefined,
+  top = undefined,
+  right = undefined,
+  bottom = undefined,
+  horizontal = undefined,
+  vertical = undefined,
+  ...props
+}) => {
+  const styles = getStyle({
+    size,
+    all,
+    left,
+    top,
+    right,
+    bottom,
+    horizontal,
+    vertical,
+  })
+  return (
+    <div style={{ ...styles, ...style }} {...props}>
+      {children}
+    </div>
+  )
+}
+
+export default component
---
+++ src/layout/Padding/index.js
@@ -0,0 +1,15 @@
+import { withStyles } from '@material-ui/styles'
+import component from './component'
+
+//
+// style,
+// size,
+// all = undefined,
+// left = undefined,
+// top = undefined,
+// right = undefined,
+// bottom = undefined,
+// horizontal = undefined,
+// vertical = undefined,
+//
+export default component
