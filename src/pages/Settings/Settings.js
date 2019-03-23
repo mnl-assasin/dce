453cc859870f8a14851e2eed8aee7734f8c1ae74
Jay Lord P. Torres <isens.jaylord@gmail.com>
2019-03-23 20:47:16 +0800

    fix: setting ui

--- src/pages/Settings/Settings.js
+++ src/pages/Settings/Settings.js
@@ -1,22 +1,26 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react'
+import PropTypes from 'prop-types'
 
-import { withStyles } from "@material-ui/core/styles";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import Divider from "@material-ui/core/Divider";
-import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight";
+import { withStyles } from '@material-ui/core/styles'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import Divider from '@material-ui/core/Divider'
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight'
 
-import BasePage from "../../common/BasePage";
-import { Page } from "../../common";
-import { Navbar } from "../../components";
+import BasePage from '../../common/BasePage'
+import { Page } from '../../layout'
 import { goTo as navigate } from '../../services/navigation'
-import { withAppContext } from "../../services/Providers/AppStateContext";
+import { withAppContext } from '../../services/Providers/AppStateContext'
 
-import styles from "./styles";
+import styles from './styles'
 
+const title = 'Settings'
+const navigationProps = {
+  title,
+  backButton: true,
+}
 
 // this will ensure that the render will not make new init every render for child
 // this must be partenered will pureComponent
@@ -25,7 +29,6 @@ import styles from "./styles";
 // };
 
 class Settings extends BasePage {
-  title = "Settings"
   settingItems = [
     // {
     //   title: 'Login and Security',
@@ -33,47 +36,50 @@ class Settings extends BasePage {
     //   onClick: () => navigate(BasePage.constants.route.SECURITY)
     // },
     {
-      title: "Network",
+      title: 'Network',
       getValue: () =>
         // this will get @example: homestead #blocknumber
-        this.props.AppContext[BasePage.constants.storage.ACTIVE_PROVIDER_NAME] + (
-                this.props.AppContext[BasePage.constants.storage.ACTIVE_PROVIDER_BlOCKNUMBER] ?
-                ' #' + this.props.AppContext[BasePage.constants.storage.ACTIVE_PROVIDER_BlOCKNUMBER] : ''),
-      onClick: () => navigate(BasePage.constants.route.NETWORK)
-    }
+        this.props.AppContext[BasePage.constants.storage.ACTIVE_PROVIDER_NAME] +
+        (this.props.AppContext[
+          BasePage.constants.storage.ACTIVE_PROVIDER_BlOCKNUMBER
+        ]
+          ? ' #' +
+            this.props.AppContext[
+              BasePage.constants.storage.ACTIVE_PROVIDER_BlOCKNUMBER
+            ]
+          : ''),
+      onClick: () => navigate(BasePage.constants.route.NETWORK),
+    },
   ]
 
   render() {
-    const { classes } = this.props;
+    const { classes } = this.props
     return (
-      <Page>
-        <Navbar title={this.title} backButton />
-        <div>
-          <List className={classes.root}>
-            {this.settingItems.map((item, index) => (
-              <React.Fragment key={index}>
-                <ListItem key={index} button onClick={item.onClick}>
-                  <ListItemText
-                    primary={item.title}
-                    secondary={item.getValue()}
-                  />
-                  <ListItemIcon>
-                    <KeyboardArrowRight />
-                  </ListItemIcon>
-                </ListItem>
-                <Divider />
-              </React.Fragment>
-            ))}
-          </List>
-        </div>
+      <Page navigationProps={navigationProps}>
+        <List className={classes.root}>
+          {this.settingItems.map((item, index) => (
+            <React.Fragment key={index}>
+              <ListItem key={index} button onClick={item.onClick}>
+                <ListItemText
+                  primary={item.title}
+                  secondary={item.getValue()}
+                />
+                <ListItemIcon>
+                  <KeyboardArrowRight />
+                </ListItemIcon>
+              </ListItem>
+              <Divider />
+            </React.Fragment>
+          ))}
+        </List>
       </Page>
-    );
+    )
   }
 }
 
 Settings.propTypes = {
   AppContext: PropTypes.object.isRequired, // withAppContext
-  classes: PropTypes.object.isRequired // withStyles
-};
+  classes: PropTypes.object.isRequired, // withStyles
+}
 
-export default withStyles(styles)(withAppContext(Settings));
+export default withStyles(styles)(withAppContext(Settings))
