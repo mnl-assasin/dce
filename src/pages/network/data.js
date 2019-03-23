eaddf68478570b9b22ba028ec69dea2ee2434a56
Jay Lord P. Torres <isens.jaylord@gmail.com>
2019-03-23 21:48:49 +0800

    network: data

---
+++ src/pages/network/data.js
@@ -0,0 +1,26 @@
+export const settingItems = [
+  {
+    _id: 'mainnet',
+    title: 'mainnet',
+  },
+  {
+    _id: 'homestead',
+    title: 'homestead',
+  },
+  {
+    _id: 'rinkeby',
+    title: 'rinkeby',
+  },
+  {
+    _id: 'ropsten',
+    title: 'ropsten',
+  },
+  {
+    _id: 'kovan',
+    title: 'kovan',
+  },
+  {
+    _id: 'goerli',
+    title: 'goerli',
+  },
+]
