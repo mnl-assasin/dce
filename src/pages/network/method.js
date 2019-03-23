a43f3bc2b7307f4d441cf87775640b7d2c67eb80
Jay Lord P. Torres <isens.jaylord@gmail.com>
2019-03-23 21:49:25 +0800

    network method

---
+++ src/pages/network/method.js
@@ -0,0 +1,34 @@
+import * as storage from '../../constants/storage'
+import * as route from '../../constants/route'
+
+export const getIsActive = appContext => network => {
+  if (!network._id) return false
+  return network._id === appContext[storage.ACTIVE_PROVIDER_ID]
+}
+
+export const setIsActive = (
+  appContext,
+  navigate,
+  setBlockNumber,
+  setError
+) => network => {
+  if (!network._id) {
+    console.log('invalid network id provided')
+    return false
+  }
+
+  if (network._id === appContext[storage.ACTIVE_PROVIDER_ID]) {
+    return ''
+  }
+  setBlockNumber(
+    network._id,
+    value =>
+      appContext.persist({
+        [storage.ACTIVE_PROVIDER_ID]: network._id,
+        [storage.ACTIVE_PROVIDER_NAME]: network.title,
+        [storage.ACTIVE_PROVIDER_BlOCKNUMBER]: String(value),
+      }),
+    error => setError({ blockNumber: '' })
+  )
+  return navigate(route.SETTING)
+}
