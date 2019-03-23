60853488516cc0897451a60a16797ee009f44755
Jay Lord P. Torres <isens.jaylord@gmail.com>
2019-03-23 15:51:37 +0800

    layout: entry point layout

---
+++ src/layout/index.js
@@ -0,0 +1,7 @@
+import Center from './Center'
+import Padding from './Padding'
+import Page from './Page'
+import Tab from './Tab'
+import TabContent from './TabContent'
+
+export { Center, Padding, Page, Tab, TabContent }
