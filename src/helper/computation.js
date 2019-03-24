94c03c073723b5e9510da27951e314912f3d667f
Jay Lord P. Torres <isens.jaylord@gmail.com>
2019-03-24 15:26:33 +0800

    helper: updated price computations

--- src/helper/computation.js
+++ src/helper/computation.js
@@ -1,13 +1,21 @@
-export const convertedPricePerValue = (base, amount) => {
+export const convertedPricePerValue = (base, amount, isString = true) => {
   try {
     const result = parseFloat(base) * parseFloat(amount)
     if (isNaN(result)) {
-      return ''
+      return isString ? '' : 0
     }
-    return String(result.toFixed(4))
+    return isString ? String(result.toFixed(4)) : result.toFixed(4)
   } catch (e) {
     console.log(e)
-    return ''
+    return isString ? '' : 0
   }
+}
 
+//this will get USER_WALLETS type
+export const computeTotalCoinValue = (base, wallets = {}) => {
+  return Object.keys(wallets).reduce(
+    (total, next, index, item) =>
+      total + convertedPricePerValue(base, wallets[next].amount, false),
+    0
+  )
 }
