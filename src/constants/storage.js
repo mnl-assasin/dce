de994fe5fd3e1ce264697b1c717e2c909b430e55
Jay Lord P. Torres <isens.jaylord@gmail.com>
2019-03-24 13:50:08 +0800

    storage note for multi wallets in app

--- src/constants/storage.js
+++ src/constants/storage.js
@@ -1,10 +1,28 @@
+// usera auth
 export const IS_LOGGED = 'isLogged'
 export const IS_SET_MNEMONIC = 'is_mnemonic_set'
 export const IS_SET_PASSWORD = 'is_password_set'
 export const PASSWORD = 'password'
+
+// app setting
 export const ACTIVE_PROVIDER_ID = 'activeProviderId'
 export const ACTIVE_PROVIDER_NAME = 'ropsten'
 export const ACTIVE_PROVIDER_BlOCKNUMBER = ''
+
+// wallets config
+export const USER_WALLETS = 'Wallets' // this is structure
+//
+// note: USER_WALLETS.USER_WALLETS or USER_WALLETS[USER_WALLETS]
+//       USER_WALLETS is not an array for fastest rendering
+//
+// USER_WALLETS {
+//   WALLET_ADDRESS: {
+//     WALLET_MNEMONIC,
+//     WALLET_ADDRESS,
+//     WALLET_PRIVATE_KEY,
+//     WALLET_PUBLIC_KEY
+//   }
+// }
 
 export const WALLET_MNEMONIC = 'mnemonic'
 export const WALLET_ADDRESS = 'address'
