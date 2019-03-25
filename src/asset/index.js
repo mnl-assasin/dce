452d1455b1c3317e4184158e0e4136196de38915
Jay Lord P. Torres <isens.jaylord@gmail.com>
2019-03-25 21:58:12 +0800

    asset entry point

--- src/asset/index.js
+++ src/asset/index.js
@@ -2,10 +2,29 @@ import EtherImage from './icon/icon_ether.png'
 import BitImage from './icon/icon_bitcoin.png'
 import Logo from './logo.png'
 
+import CoinMenu from './icon/icon_coin_menu.png'
+import Add from './icon/icon_add.png'
+import DappActive from './icon/icon_dapp_active.png'
+import DappInactive from './icon/icon_dapp_inactive.png'
+import Ether from './icon/icon_ether_small.png'
+import Hamburger from './icon/icon_hamburger.png'
+import WalletGreen from './icon/icon_wallet_green.png'
+import WalletViolet from './icon/icon_wallet_violet.png'
+
+// wallet icons
 export const icon = {
-  ETHER: EtherImage,
+  ETHER: Ether,
   BIT: BitImage,
 }
 export {
-  Logo
+  Logo,
+  EtherImage,
+  CoinMenu,
+  Add,
+  DappActive,
+  DappInactive,
+  Ether,
+  Hamburger,
+  WalletGreen,
+  WalletViolet,
 }
