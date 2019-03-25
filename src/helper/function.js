79ebd5f46a8ee1f32c530596c9fceeeb850a825f
Jay Lord P. Torres <isens.jaylord@gmail.com>
2019-03-25 20:53:12 +0800

    helper: sorting object

--- src/helper/function.js
+++ src/helper/function.js
@@ -3,4 +3,15 @@ export const emptyMethod = string => {}
 export const isDefined = value => {
   return typeof value !== 'undefined' && value !== null
 }
-  
+
+export const orderObjectByKey = obj => {
+  return Object.entries(obj)
+    .sort()
+    .reduce(
+      (_sortedObj, [k, v]) => ({
+        ..._sortedObj,
+        [k]: v,
+      }),
+      {}
+    )
+}
