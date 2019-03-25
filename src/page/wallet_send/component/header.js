b6616988411a34ad403956c067ef70b5e0731e95
Jay Lord P. Torres <isens.jaylord@gmail.com>
2019-03-26 00:40:51 +0800

    wallet_send: header

---
+++ src/page/wallet_send/component/header.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+import { Text, Icon } from '../../../components'
+import { boxShadow } from '../../../constants/style'
+import { ContentCopy } from '../../../asset'
+import { Page, Padding, Tab, TabContent } from '../../../layout'
+
+export default ({ title = '', label = '' }) => (
+  <Tab title="SEND">
+    <TabContent direction="column">
+      <div style={style.cardstyle}>
+        <div>
+          <Text style={style.title}>{label}</Text>
+          <Text style={style.address}>{title}</Text>
+        </div>
+        <Icon src={ContentCopy} size={15} />
+      </div>
+    </TabContent>
+  </Tab>
+)
+
+const style = {
+  cardstyle: {
+    borderRadius: 10,
+    backgroundColor: 'white',
+    boxShadow: boxShadow,
+    flexDirection: 'row',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: '10px 16px',
+  },
+  title: { fontSize: 9, fontWeight: 'bold', color: 'gray' },
+  address: { fontSize: 10.3, fontWeight: 'bold' },
+}
