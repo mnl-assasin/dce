963ae3744c23e98211fc02bff3058b1b7070d5d2
Jay Lord P. Torres <isens.jaylord@gmail.com>
2019-03-26 21:25:55 +0800

    wallet_send: ui

--- src/page/wallet_send/wallet_send.jsx
+++ src/page/wallet_send/wallet_send.jsx
@@ -7,15 +7,16 @@ import AddCircleOutline from '@material-ui/icons/AddCircleOutline'
 import FileCopy from '@material-ui/icons/FileCopy'
 import InputAdornment from '@material-ui/core/InputAdornment'
 import IconButton from '@material-ui/core/IconButton'
-import InputBase from '@material-ui/core/InputBase';
+import InputBase from '@material-ui/core/InputBase'
 
 import { Page } from '../../layout'
-import { Icon } from '../../components'
+import { Text, Icon, InputTextBox, PrimaryButton } from '../../components'
 import { estimateFee, sendWallet } from '../../hof'
 import { Row } from '../../common'
+
 import { useTextbox } from '../../hook'
 import { inputTypes } from '../../constants/types'
-import { Add } from '../../asset'
+import { ContentCopy, Add } from '../../asset'
 import { AppContextObject } from '../../services/Providers/AppStateContext'
 import * as storage from '../../constants/storage'
 import Header from './component/header'
@@ -83,100 +84,143 @@ const WalletSend = props => {
     <Page navigationProps={navigationProps}>
       <Header title={yourAddresss.value} label={'Your Address ' + title} />
       <form className={classes.container} noValidate autoComplete="off">
-        <InputBase
-          className={classes.textField}
-          variant="outlined"
-          type="text"
-          name="sendTo"
-          label="Send To"
-          {...sendTo}
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <Icon src={Add} size={25} />
-              </InputAdornment>
-            ),
-          }}
+        <InputTextBox
+          placeholder="Send To"
+          hasIcon
+          renderIcon={<Icon src={Add} size={25} />}
         />
-
+        <Divider />
         <Row>
-          <div className={classes.sendTo} />
-          <div className={classes.addIcon}>
-            <AddCircleOutline fontSize="inherit" />
-          </div>
+          <InputTextBox
+            placeholder="Amount"
+            renderIcon={<Icon src={Add} size={25} hasIcon />}
+          />
+          <Divider />
+          <InputTextBox
+            placeholder="USD"
+            renderIcon={<Icon src={Add} size={25} hasIcon />}
+          />
         </Row>
-        <Row className={classes.textField}>
-          <div className={classes.amount}>
-            <TextField
-              name="amount"
-              label="Amount"
-              variant="outlined"
-              className={classes.textField}
-              type={inputTypes.text}
-              {...amount}
+        <Divider />
+        <InputTextBox
+          placeholder="Gas Limit (recomended)"
+          hasIcon
+          renderIcon={
+            <Icon
+              iconName="local_gas_station"
+              style={{ backgroundColorcolor: '#ee5791' }}
+              size={25}
             />
-          </div>
-          <div className={classes.usd}>
-            <TextField
-              name="usd"
-              label="USD"
-              variant="outlined"
-              className={classes.textField}
-              type={inputTypes.text}
-              {...usd}
-            />
-          </div>
-        </Row>
-        <Row>
-          <div className={classes.gasLimit}>
-            <TextField
-              name="gasLimit"
-              label="Gas Limit (recomended)"
-              variant="outlined"
-              className={classes.textField}
-              type={inputTypes.text}
-              {...gasLimit}
-            />
-          </div>
-          <div className={classes.gasIcon}>
-            <LocalGasStation fontSize="inherit">
-              local_gas_station
-            </LocalGasStation>
-          </div>
-        </Row>
-        <TextField
-          name="transaction"
-          label="Tansaction Data (optional)"
-          variant="outlined"
-          className={classes.textField}
-          type={inputTypes.text}
-          {...transaction}
+          }
         />
-        <Typography variant="caption" gutterBottom>
-          estimate fee: {fee}
-        </Typography>
-        <div className={classes.buttonHolder}>
-          <Button
-            variant="outlined"
-            color="primary"
-            size="large"
-            onClick={onClickEstimate}
-          >
-            estimate
-          </Button>
+        <Divider />
+        <InputTextBox
+          placeholder="Tansaction Data (optional)"
+          renderIcon={<Icon src={Add} size={25} hasIcon />}
+        />
+        <Divider />
+        <Divider />
+        <Divider />
+        <Divider />
 
-          <Button
-            variant="outlined"
-            color="primary"
-            size="large"
-            onClick={onClickSubmit}
-          >
-            send
-          </Button>
-        </div>
+        <PrimaryButton type="primary" title="">
+          SUBMTI
+        </PrimaryButton>
       </form>
     </Page>
   )
 }
 
+const Divider = props => <span style={{ padding: '0.3rem' }} />
 export default WalletSend
+// <InputBase
+//   className={classes.textField}
+//   variant="outlined"
+//   type="text"
+//   name="sendTo"
+//   label="Send To"
+//   {...sendTo}
+//   InputProps={{
+//     endAdornment: (
+//       <InputAdornment position="end">
+//         <Icon src={Add} size={25} />
+//       </InputAdornment>
+//     ),
+//   }}
+// />
+//
+// <Row>
+//   <div className={classes.sendTo} />
+//   <div className={classes.addIcon}>
+//     <AddCircleOutline fontSize="inherit" />
+//   </div>
+// </Row>
+// <Row className={classes.textField}>
+//   <div className={classes.amount}>
+//     <TextField
+//       name="amount"
+//       label="Amount"
+//       variant="outlined"
+//       className={classes.textField}
+//       type={inputTypes.text}
+//       {...amount}
+//     />
+//   </div>
+//   <div className={classes.usd}>
+//     <TextField
+//       name="usd"
+//       label="USD"
+//       variant="outlined"
+//       className={classes.textField}
+//       type={inputTypes.text}
+//       {...usd}
+//     />
+//   </div>
+// </Row>
+// <Row>
+//   <div className={classes.gasLimit}>
+//     <TextField
+//       name="gasLimit"
+//       label="Gas Limit (recomended)"
+//       variant="outlined"
+//       className={classes.textField}
+//       type={inputTypes.text}
+//       {...gasLimit}
+//     />
+//   </div>
+//   <div className={classes.gasIcon}>
+//     <LocalGasStation fontSize="inherit">
+//       local_gas_station
+//     </LocalGasStation>
+//   </div>
+// </Row>
+// <TextField
+//   name="transaction"
+//   label="Tansaction Data (optional)"
+//   variant="outlined"
+//   className={classes.textField}
+//   type={inputTypes.text}
+//   {...transaction}
+// />
+// <Typography variant="caption" gutterBottom>
+//   estimate fee: {fee}
+// </Typography>
+// <div className={classes.buttonHolder}>
+//   <Button
+//     variant="outlined"
+//     color="primary"
+//     size="large"
+//     onClick={onClickEstimate}
+//   >
+//     estimate
+//   </Button>
+//
+//   <Button
+//     variant="outlined"
+//     color="primary"
+//     size="large"
+//     onClick={onClickSubmit}
+//   >
+//     send
+//   </Button>
+// </div>
