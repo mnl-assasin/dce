d65e600ded661aeed796d50af1651b4474895ed9
Jay Lord P. Torres <isens.jaylord@gmail.com>
2019-03-29 00:10:33 +0800

    test_page

--- src/page/test_page/test_page.jsx
+++ src/page/test_page/test_page.jsx
@@ -1,62 +1,82 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import { withStyles } from '@material-ui/styles'
 import { Wallet, Transaction } from 'dapper'
 
 import BasePage from '../../common/BasePage'
 import { goTo } from '../../services/navigation'
-import { PrimaryButton, SmallButton } from '../../components'
+import {
+  Text,
+  Icon,
+  CardDapp,
+  InputTextBox,
+  PrimaryButton,
+  SmallButton,
+} from '../../components'
 import Button from '@material-ui/core/Button'
 import { withAppContext } from '../../services/Providers/AppStateContext'
 
-import { Text, Icon, CardDapp, InputTextBox } from '../../components'
 import { boxShadow } from '../../constants/style'
-import { ContentCopy } from '../../asset'
+import { ContentCopy, Add } from '../../asset'
 import { Page, Padding, Tab, TabContent } from '../../layout'
-
+import { Row } from '../../common'
+import WalletHeader from '../../page/wallet/component/WalletHeader'
 import Fab from '@material-ui/core/Fab'
 // 0x83287fc34Bd986A23e2C0BAaf09C898d80ff34c2
 class TestPage extends BasePage {
   render() {
+    const { classes } = this.props
     return (
-      <Page>
-        <Tab title="SEND">
-          <TabContent direction="column">
-            <div style={style.cardstyle}>
-              <div>
-                <Text style={style.title}>YOUR ADDRESS</Text>
-                <Text style={style.address}>
-                  0x83287fc34Bd986A23e2C0BAaf09C898d80ff34c2
-                </Text>
-              </div>
-              <Icon src={ContentCopy} size={15} />
-            </div>
-          </TabContent>
-        </Tab>
-        <InputTextBox />
+      <Page containerProps={{ style: { paddingLeft: 0, paddingRight: 0 } }}>
+        <div>
+          <WalletHeader
+            classes={classes}
+            networkName="MAINNET"
+            networkNumber="6784369"
+            coinPrice="123.2"
+            // userName={props.wallet[storage.WALLET_USERNAME]}
+            userName="@BLOCKBUSTER"
+            coinBase="ETH"
+          />
+        </div>
       </Page>
     )
   }
 }
+const Divider = props => <span style={{ padding: '0.3rem' }} />
 
-const style = {
-  cardstyle: {
-    borderRadius: 10,
-    backgroundColor: 'white',
-    boxShadow: boxShadow,
-    flexDirection: 'row',
+export const styles = theme => ({
+  headerHolder: {
     display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    padding: '10px 16px',
+    paddingLeft: 20,
+    paddingRight: 20,
   },
-  title: { fontSize: 9, fontWeight: 'bold', color: 'gray' },
-  address: { fontSize: 10.3, fontWeight: 'bold' },
-}
-
+  headerProvider: {
+    display: 'flex',
+    flex: 1,
+    justifyContent: 'flex-start',
+    flexDirection: 'column',
+  },
+  headerCoin: {
+    display: 'flex',
+    flex: 1,
+    justifyContent: 'flex-start',
+    textAlign: 'center',
+    flexDirection: 'column',
+    paddingTop: theme.spacing.unit * 2,
+  },
+  headerUser: {
+    display: 'flex',
+    flex: 1,
+    justifyContent: 'flex-start',
+    flexDirection: 'column',
+    textAlign: 'right',
+  },
+})
 // TestPage.propTypes = {
 //   AppContext: PropTypes.object.isRequired, // withAppContext
 //   // classes: PropTypes.object.isRequired // withStyles
 // };
 
-export default withAppContext(TestPage)
+export default withAppContext(withStyles(styles)(TestPage))
