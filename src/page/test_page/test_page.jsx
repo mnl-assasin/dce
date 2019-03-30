f1a3ebc4bb50ec77a3ad7e8ea5c0bff9220e3a9a
Jay Lord P. Torres <isens.jaylord@gmail.com>
2019-03-30 21:57:11 +0800

    test

--- src/page/test_page/test_page.jsx
+++ src/page/test_page/test_page.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import { withStyles } from '@material-ui/styles'
-import { Wallet, Transaction } from 'dapper'
 import BasePage from '../../common/BasePage'
 import { goTo } from '../../services/navigation'
 import {
@@ -24,88 +23,32 @@ import { Row } from '../../common'
 import WalletHeader from '../../page/wallet/component/WalletHeader'
 import Fab from '@material-ui/core/Fab'
 // 0x83287fc34Bd986A23e2C0BAaf09C898d80ff34c2
+import * as storage from '../../constants/storage'
+import createHDWallet from '../../hof/create_hd_wallet'
+import { propertyCount } from '../../helper/function'
+
 class TestPage extends BasePage {
+  start = () => {
+    createHDWallet(this.props.AppContext)(
+      // wallet mnemonic
+      'talk cook energy elite dragon mom copy dirt wall kangaroo poem true',
+      // path
+      0
+      // propertyCount(this.props.AppContext[storage.USER_WALLETS])
+    )
+  }
   render() {
-    const { classes } = this.props
+    console.log(this.props.AppContext)
     return (
       <Page containerProps={{ style: { paddingLeft: 0, paddingRight: 0 } }}>
-        <div>
-          <WalletHeader
-            classes={classes}
-            networkName="MAINNET"
-            networkNumber="6784369"
-            coinPrice="123.2"
-            // userName={props.wallet[storage.WALLET_USERNAME]}
-            userName="@BLOCKBUSTER"
-            coinBase="ETH"
-          />
-          <div className={classes.container}>
-            <div className={classes.content}>
-              <span>sdfsd</span>
-              <span>
-                <Radio style={{ color: '#46a585' }} />
-              </span>
-            </div>
-            <hr
-              style={{
-                border: '.5px solid #dadada',
-                margin: 0,
-                marginLeft: 12,
-              }}
-            />
-            <div className={classes.content}>
-              <span>sdfsd</span>
-              <span>
-                <Radio style={{ color: '#46a585' }} />
-              </span>
-            </div>
-          </div>
-        </div>
+        CREATE WALLET
+        <PrimaryButton type="primary" onClick={this.start}>
+          create
+        </PrimaryButton>
       </Page>
     )
   }
 }
-const Divider = props => <span style={{ padding: '0.3rem' }} />
 
-export const styles = theme => ({
-  headerHolder: {
-    display: 'flex',
-    paddingLeft: 20,
-    paddingRight: 20,
-  },
-  headerProvider: {
-    display: 'flex',
-    flex: 1,
-    justifyContent: 'flex-start',
-    flexDirection: 'column',
-  },
-  headerCoin: {
-    display: 'flex',
-    flex: 1,
-    justifyContent: 'flex-start',
-    textAlign: 'center',
-    flexDirection: 'column',
-    paddingTop: theme.spacing.unit * 2,
-  },
-  headerUser: {
-    display: 'flex',
-    flex: 1,
-    justifyContent: 'flex-start',
-    flexDirection: 'column',
-    textAlign: 'right',
-  },
-  container: { border: '1px solid #dadada' },
-  content: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    paddingLeft: 12,
-  },
-})
-// TestPage.propTypes = {
-//   AppContext: PropTypes.object.isRequired, // withAppContext
-//   // classes: PropTypes.object.isRequired // withStyles
-// };
-
-export default withAppContext(withStyles(styles)(TestPage))
+// export default withAppContext(withStyles(styles)(TestPage))
+export default withAppContext(TestPage)
