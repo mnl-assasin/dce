737e0e8a2021f9e68a186808f24d0443792c17c1
Jay Lord P. Torres <isens.jaylord@gmail.com>
2019-04-07 11:22:08 +0800

    fix: page background color

--- src/layout/page/styles.js
+++ src/layout/page/styles.js
@@ -3,7 +3,7 @@ export default theme => ({
     width: '100%',
     height: '100%',
     maxWidth: 360,
-    backgroundColor: theme.palette.background.paper,
+    backgroundColor:'#F9F9F9' ,
   },
   container: {
     padding: '14px 26px ',
