e01cd87b069302356ed2c4a65a763f14d04f4141
Jay Lord P. Torres <isens.jaylord@gmail.com>
2019-04-13 06:10:54 +0800

    input_text_box: styling

--- src/components/input_text_box/index.js
+++ src/components/input_text_box/index.js
@@ -1,21 +1,25 @@
 import React from 'react'
+import { TextStyles  } from '../../constants/style'
+
+const styles = {
+  input: (hasIcon = false) => ({
+    ...TextStyles.style3,
+    backgroundColor: 'white',
+    padding: '0.75rem 0.5rem',
+    borderRadius: 2,
+    borderColor: '#dedede',
+    borderStyle: 'solid',
+    borderWidth: 1,
+    width: '100%',
+    paddingRight: hasIcon ? 40 : '1rem',
+  })
+}
 
 export default ({ hasIcon = false, renderIcon = null, style = {}, ...props }) => (
   <div style={{ flexDirection: 'row', position: 'relative' }}>
     <input
       type="text"
-      style={{
-        fontSize: 16,
-        color: 'gray',
-        backgroundColor: 'white',
-        padding: '0.75rem 0.5rem',
-        borderRadius: 2,
-        borderColor: '#dedede',
-        borderStyle: 'solid',
-        borderWidth: 1,
-        width: '100%',
-        paddingRight: hasIcon ? 40 : '1rem',
-      }}
+      style={styles.input(hasIcon)}
       placeholder=""
       {...props}
     />
