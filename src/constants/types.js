32673eb87a2a383df996854441806ae890495801
Jay Lord P. Torres <isens.jaylord@gmail.com>
2019-05-09 02:09:48 +0800

    constant:  inputTypes: add text area

--- src/constants/types.js
+++ src/constants/types.js
@@ -1,10 +1,11 @@
 export const inputTypes = {
   text: 'text',
   password: 'password',
+  textarea: 'textarea',
 }
 
 export const displayTypes = {
   quantity: 'quantity', // 'ETH'
   value: 'value', // '$'
-  default: 'quantity'
+  default: 'quantity',
 }
