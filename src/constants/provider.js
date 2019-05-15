91f4e00176bb2da28232c388e423835912d2059a
Jay Lord P. Torres <isens.jaylord@gmail.com>
2019-05-16 01:00:01 +0800

    constant: providers

---
+++ src/constants/provider.js
@@ -0,0 +1,26 @@
+export const Providers = {
+  mainnet: {
+    _id: 'mainnet',
+    title: 'mainnet',
+  },
+  homestead: {
+    _id: 'homestead',
+    title: 'homestead',
+  },
+  rinkeby: {
+    _id: 'rinkeby',
+    title: 'rinkeby',
+  },
+  ropsten: {
+    _id: 'ropsten',
+    title: 'ropsten',
+  },
+  kovan: {
+    _id: 'kovan',
+    title: 'kovan',
+  },
+  goerli: {
+    _id: 'goerli',
+    title: 'goerli',
+  },
+}
