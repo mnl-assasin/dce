409d8739657d71799b5ed599127927b993ce5069
Jay Lord P. Torres <isens.jaylord@gmail.com>
2019-05-21 07:56:55 +0800

    component: dapp

--- src/components/CardDapp/component.jsx
+++ src/components/CardDapp/component.jsx
@@ -11,11 +11,18 @@ class CardDapp extends React.PureComponent {
       textTransform = 'none',
       componentIcon,
       isActive,
-      onAddDapp
+      onAddDapp,
+      onPress = () => {},
+      param,
     } = this.props
 
     return (
-      <Padding horizontal={4} style={{ display: 'flex', flexDirection: 'row' }} onClick={onAddDapp}>
+      <Padding
+        horizontal={4}
+        style={{ display: 'flex', flexDirection: 'row' }}
+        // onClick={onAddDapp}
+        onClick={() => onPress(param)}
+      >
         <Padding style={styles.container(isActive)} vertical={16}>
           {componentIcon}
           <Padding horizontal={4}>
--- src/page/dashboard/component/dapp_section.jsx
+++ src/page/dashboard/component/dapp_section.jsx
@@ -2,30 +2,40 @@ import React from 'react'
 import { Padding, Tab, TabContent } from '../../../layout'
 import { FabButton, CardDapp, Icon } from '../../../components'
 import { DappInactive, DappActive, WalletGreen } from '../../../asset'
+import { DAPP } from '../../../constants/storage'
 
-export default ({ onAddDapp }) => {
+export default ({ onAddDapp, dapps = [], onDappItem }) => {
   return (
     <Tab
       title="dapps"
       renderButton={
-        <React.Fragment >
+        <React.Fragment>
           <Padding horizontal={4}>
             <Icon src={WalletGreen} size={50} />
           </Padding>
         </React.Fragment>
       }
     >
-      <TabContent direction="row" style={{overflowX: 'auto'}}>
+      <TabContent direction="row" style={{ overflowX: 'auto' }}>
         <CardDapp
-          onAddDapp={onAddDapp}
+          // onAddDapp={onAddDapp}
+          onPress={onAddDapp}
           isActive
           textTransform="uppercase"
           title="new dapp"
-          componentIcon={
-            <Icon src={DappActive} size={56} />
-          }
+          componentIcon={<Icon src={DappActive} size={56} />}
         />
-    </TabContent>
+        {Object.keys(dapps).map((_id, index) => (
+          <CardDapp
+            key={_id}
+            textTransform="uppercase"
+            param={_id}
+            onPress={onDappItem}
+            title={dapps[_id][DAPP.DAPP_NAME]}
+            componentIcon={<Icon src={DappInactive} size={56} />}
+          />
+        ))}
+      </TabContent>
     </Tab>
   )
 }
