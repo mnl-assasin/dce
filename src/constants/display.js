5cad7b2ea7abd6dce5902ea25ff5bfcc41e569d5
Jay Lord P. Torres <isens.jaylord@gmail.com>
2019-05-22 09:18:25 +0800

    constant: maximuna count for session dipslay in dapp_view

---
+++ src/constants/display.js
@@ -0,0 +1,1 @@
+export const DappViewMaximumSessionCount = 6
--- src/page/dapp_view/component/session.jsx
+++ src/page/dapp_view/component/session.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Item from './item'
+import { DappViewMaximumSessionCount } from '../../../constants/display'
 
 export default ({ classes, sessions = [] }) => {
   return (
     <div className={classes.blocWrapper}>
       <div className={classes.resultTitle}>SESSION HISTORY</div>
-      {Object.keys(sessions.slice(0, 10)).map(k => {
+      {Object.keys(sessions.slice(0, DappViewMaximumSessionCount)).map(k => {
         return (
           <Item
             key={k}
